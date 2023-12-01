2de40748bfa4f8a78e0bb18fa923f42d578f4d25
kubalx <105309675+kubalx@users.noreply.github.com>
2023-12-01 20:02:30 +0100

    Poprawki dot. multiplayer

--- ConsoleSnake/ConsoleSnake/Program.cs
+++ ConsoleSnake/ConsoleSnake/Program.cs
@@ -1,16 +1,25 @@
-﻿namespace ConsoleSnake
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleSnake
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var snake1 = new List<Point> { new Point(10, 5) };
-            var food = GetFood(snake1, 50, 20);
+            var snake1 = new List<Point> { new Point(10, 5) }; 
+            var snake2 = new List<Point> { new Point(30, 15) };
+
+            var food = GetFood(snake1.Concat(snake2).ToList(), 50, 20); 
             var direction1 = Direction.Right;
-            var boardWidth = 50;
+            var direction2 = Direction.Left;
+            var boardWidth = 50; 
             var boardHeight = 20;
-            var score1 = 0; // Inicjalizacja wyniku poza pętlą główną
-            var random = new Random();
+            var score1 = 0;
+            var score2 = 0;
+            var gameOver = false;
 
             Console.CursorVisible = false;
             while (true)
@@ -19,50 +28,58 @@
                 {
                     var key = Console.ReadKey(true).Key;
                     direction1 = ChangeDirection(key, direction1, true);
+                    direction2 = ChangeDirection(key, direction2, false);
                 }
 
                 MoveSnake(snake1, direction1);
+                MoveSnake(snake2, direction2);
 
-                if (snake1[0].X == food.X && snake1[0].Y == food.Y)
+                if (snake1[0].Equals(food))
                 {
                     snake1.Add(new Point(snake1[^1].X, snake1[^1].Y));
-                    food = GetNewFoodPosition(snake1, boardWidth, boardHeight, random);
-                    score1++; // Zwiększenie wyniku po zdobyciu jedzenia
+                    food = GetFood(snake1.Concat(snake2).ToList(), boardWidth, boardHeight);
+                    score1++; 
+                }
+                else if (snake2[0].Equals(food))
+                {
+                    snake2.Add(new Point(snake2[^1].X, snake2[^1].Y));
+                    food = GetFood(snake1.Concat(snake2).ToList(), boardWidth, boardHeight);
+                    score2++; 
                 }
 
-                if (CheckCollision(snake1, boardWidth, boardHeight))
+                if (CheckCollision(snake1) || CheckCollision(snake2))
                 {
-                    snake1 = new List<Point> { new Point(10, 5) };
-                    food = GetFood(snake1, boardWidth, boardHeight);
+                    Console.Clear();
+                    Console.SetCursorPosition(boardWidth / 2 - 10, boardHeight / 2);
+                    Console.Write("Collision! Scores - Player1: {0}, Player2: {1}", score1, score2);
+                    Thread.Sleep(2000); // Opóźnienie przed kontynuacją
+
+                    // Resetowanie pozycji węży i wyników
+                    snake1.Clear();
+                    snake1.Add(new Point(10, 5));
+                    snake2.Clear();
+                    snake2.Add(new Point(30, 15));
+                    score1 = 0;
+                    score2 = 0;
+                    food = GetFood(snake1.Concat(snake2).ToList(), boardWidth, boardHeight);
                     direction1 = Direction.Right;
-                    // score1 = 0; // Nie ustawiamy wyniku na 0 po kolizji
+                    direction2 = Direction.Left;
+                    continue; // Kontynuacja pętli gry
                 }
 
                 Console.Clear();
-                DrawBoard(snake1, food, boardWidth, boardHeight);
-                Console.WriteLine($"Score1: {score1}"); // Wyświetlenie aktualnego wyniku
+                DrawBoard(snake1, snake2, food, boardWidth, boardHeight);
+                Console.WriteLine($"Score1: {score1} Score2: {score2}");
                 Thread.Sleep(200);
             }
+
+            Console.SetCursorPosition(boardWidth / 2, boardHeight / 2);
+            Console.Write("Game Over! Final Scores - Player1: {0}, Player2: {1}", score1, score2);
         }
-
-
-
-
 
         static Point GetFood(List<Point> snake, int width, int height)
         {
             var random = new Random();
-            Point food;
-            do
-            {
-                food = new Point(random.Next(width), random.Next(height));
-            }
-            while (snake.Any(s => s.Equals(food)));
-            return food;
-        }
-
-        static Point GetNewFoodPosition(List<Point> snake, int width, int height, Random random)
-        {
             Point food;
             do
             {
@@ -89,15 +106,19 @@
             };
         }
 
-        static void DrawBoard(List<Point> snake, Point food, int width, int height)
+        static void DrawBoard(List<Point> snake1, List<Point> snake2, Point food, int width, int height)
         {
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    if (snake.Any(s => s.X == x && s.Y == y))
+                    if (snake1.Any(s => s.X == x && s.Y == y))
                     {
                         Console.Write("*");
+                    }
+                    else if (snake2.Any(s => s.X == x && s.Y == y))
+                    {
+                        Console.Write("+");
                     }
                     else if (food.X == x && food.Y == y)
                     {
@@ -112,21 +133,35 @@
             }
         }
 
-        static bool CheckCollision(List<Point> snake, int width, int height)
+        static bool CheckCollision(List<Point> snake)
         {
-            return snake.Skip(1).Any(s => s.Equals(snake[0])) || snake[0].X < 0 || snake[0].Y < 0 || snake[0].X >= width || snake[0].Y >= height;
+            return snake.Skip(1).Any(s => s.Equals(snake[0]));
         }
 
         static Direction ChangeDirection(ConsoleKey key, Direction currentDirection, bool isPlayer1)
         {
-            return key switch
+            if (isPlayer1)
             {
-                ConsoleKey.W when currentDirection != Direction.Down => Direction.Up,
-                ConsoleKey.S when currentDirection != Direction.Up => Direction.Down,
-                ConsoleKey.A when currentDirection != Direction.Right => Direction.Left,
-                ConsoleKey.D when currentDirection != Direction.Left => Direction.Right,
-                _ => currentDirection
-            };
+                return key switch
+                {
+                    ConsoleKey.W when currentDirection != Direction.Down => Direction.Up,
+                    ConsoleKey.S when currentDirection != Direction.Up => Direction.Down,
+                    ConsoleKey.A when currentDirection != Direction.Right => Direction.Left,
+                    ConsoleKey.D when currentDirection != Direction.Left => Direction.Right,
+                    _ => currentDirection
+                };
+            }
+            else
+            {
+                return key switch
+                {
+                    ConsoleKey.UpArrow when currentDirection != Direction.Down => Direction.Up,
+                    ConsoleKey.DownArrow when currentDirection != Direction.Up => Direction.Down,
+                    ConsoleKey.LeftArrow when currentDirection != Direction.Right => Direction.Left,
+                    ConsoleKey.RightArrow when currentDirection != Direction.Left => Direction.Right,
+                    _ => currentDirection
+                };
+            }
         }
     }
 
